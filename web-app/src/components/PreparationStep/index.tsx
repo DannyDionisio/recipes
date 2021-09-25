bf73f3ac3f9336137bee02a4abb687bc4be1d14f
Daniela Dionisio <dannyy.dionisio@gmail.com>
2021-09-25 15:33:22 +0100

    last structure update

---
+++ web-app/src/components/PreparationStep/index.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+import BlenderIcon from "../../assets/images/blender-icon.svg";
+
+import Input from "../Input";
+import AddButton from "../AddButton";
+
+interface Step {
+  key: number;
+  value: string;
+}
+
+const PreparationStep = () => {
+  const [steps, setSteps] = useState<Step[]>([]);
+
+  function handleAddPrepStep(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    setSteps([...steps, { key: steps.length, value: "" }]);
+  }
+
+  function handleStepChange(
+    e: React.ChangeEvent<HTMLInputElement>,
+    step: Step
+  ) {
+    const stepValue = e.target.value;
+
+    const newSteps = steps.map((s) => {
+      if (s.key === step.key) {
+        return {
+          key: s.key,
+          value: stepValue,
+        };
+      }
+
+      return s;
+    });
+
+    setSteps(newSteps);
+  }
+
+  return (
+    <div className="preparation-step-container">
+      {steps.length > 0 && <p>Etapa Preparação</p>}
+
+      {steps.map((step) => (
+        <Input
+          name="preparation-step"
+          onChange={(e) => handleStepChange(e, step)}
+          value={step.value}
+          key={step.key}
+        />
+      ))}
+
+      <AddButton
+        source={BlenderIcon}
+        icon="blender-icon"
+        title="ADICIONAR ETAPA PREPARAÇÃO"
+        onClick={handleAddPrepStep}
+      />
+    </div>
+  );
+};
+
+export default PreparationStep;
